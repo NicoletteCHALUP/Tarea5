064a7afaf4c290144ea347e2a3076f92ee7c0984
Nicolette <nicolette.chalup@ucb.edu.bo>
2023-08-19 15:00:14 -0400

    7mo commit saludar  dias,tardes y noches: Bien

--- src/kbot/kbot.test.js
+++ src/kbot/kbot.test.js
@@ -1,22 +1,3 @@
-const Kbot = require("./Kbot");
-/*
-console.log("Saludar a Andrés en idioma default".toUpperCase());
-let kbot = new Kbot();
-console.log(kbot.saludar("Andrés") == "Buenos días Andrés");
-
-kbot = new Kbot("EN");
-console.log("Saludar a Andrés en ingles".toUpperCase());
-console.log(kbot.saludar("Andrés") == "Good morning Andrés");
-
-console.log("Saludar a Andrés en espanol por la tarde".toUpperCase());
-kbot = new Kbot("ES");
-console.log(kbot.saludar("Andrés", 14)=="Buenas tardes Andrés");
-
-console.log("Saludar a Juan en espanol por la noche".toUpperCase());
-kbot = new Kbot("ES");
-console.log(kbot.saludar("Juan", 20)=="Buenas noches Juan");
-*/
-
 class Kbot {
     constructor(idioma = "EN") {
         this.idioma = idioma;
@@ -32,24 +13,80 @@ class Kbot {
             } else {
                 saludo = "Buenas noches";
             }
-        } 
+        } else {
+            if (hora >= 0 && hora < 12) {
+                saludo = "Good morning";
+            } else if (hora >= 12 && hora < 18) {
+                saludo = "Good afternoon";
+            } else {
+                saludo = "Good evening";
+            }
+        }
         return `${saludo} ${nombre}`;
     }
 }
 
 describe("Saludor", () => {
-    it("deberia Saludar a Andrés en idioma default", () => {
-        let kbot = new Kbot();
-        let resultado = kbot.saludar("Andrés", 8) 
+    // Ingles
+    it("Should greet 'Good morning Andrés' in English in the morning", () => {
+        const now = new Date();
+        const mockHours = 10; // Morning time (e.g., 10:00 AM)
+        jest.spyOn(Date, 'now').mockImplementation(() => new Date(now.getFullYear(), now.getMonth(), now.getDate(), mockHours).getTime());
+        
+        let kbot = new Kbot("EN");
+        let resultado = kbot.saludar("Andrés");
+        expect(resultado).toEqual("Good morning Andrés");
+    });
+    //espanol
+    it("Should greet 'Buenos días Andrés' in Spanish in the morning", () => {
+        const now = new Date();
+        const mockHours = 10; 
+        jest.spyOn(Date, 'now').mockImplementation(() => new Date(now.getFullYear(), now.getMonth(), now.getDate(), mockHours).getTime());
+        
+        let kbot = new Kbot("ES");
+        let resultado = kbot.saludar("Andrés");
         expect(resultado).toEqual("Buenos días Andrés");
     });
 
-    it("Saludar a Andrés en espanol por la tarde", () => {
+    it("Should greet 'Good afternoon Andrés' ", () => {
+        const now = new Date();
+        const mockHours = 14; 
+        jest.spyOn(Date, 'now').mockImplementation(() => new Date(now.getFullYear(), now.getMonth(), now.getDate(), mockHours).getTime());
+        
+        let kbot = new Kbot("EN");
+        let resultado = kbot.saludar("Andrés");
+        expect(resultado).toEqual("Good afternoon Andrés");
+    });
+
+    // Español
+    it("Should greet 'Buenas tardes Andrés' ", () => {
+        const now = new Date();
+        const mockHours = 14; 
+        jest.spyOn(Date, 'now').mockImplementation(() => new Date(now.getFullYear(), now.getMonth(), now.getDate(), mockHours).getTime());
+        
         let kbot = new Kbot("ES");
-        let resultado = kbot.saludar("Andrés", 13)
+        let resultado = kbot.saludar("Andrés");
         expect(resultado).toEqual("Buenas tardes Andrés");
     });
 
-    
-  });
-  
+    it("Should greet 'Good nigth Andrés' ", () => {
+        const now = new Date();
+        const mockHours = 20; 
+        jest.spyOn(Date, 'now').mockImplementation(() => new Date(now.getFullYear(), now.getMonth(), now.getDate(), mockHours).getTime());
+        
+        let kbot = new Kbot("EN");
+        let resultado = kbot.saludar("Andrés");
+        expect(resultado).toEqual("Good nigth Andrés");
+    });
+
+    // Español
+    it("Should greet 'Buenas noches Andrés' ", () => {
+        const now = new Date();
+        const mockHours = 20; 
+        jest.spyOn(Date, 'now').mockImplementation(() => new Date(now.getFullYear(), now.getMonth(), now.getDate(), mockHours).getTime());
+        
+        let kbot = new Kbot("ES");
+        let resultado = kbot.saludar("Andrés");
+        expect(resultado).toEqual("Buenas noches Andrés");
+    });
+});
